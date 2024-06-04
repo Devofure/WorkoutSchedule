9843de524d464c1d8a1dd9a5d20604283feafb10
Erick Chavez <devofure@gmail.com>
2024-06-04 23:42:23 +0200

    added transition to the add Exercise screen

--- app/src/main/java/com/devofure/workoutschedule/ui/main/AddWorkoutScreen.kt
+++ app/src/main/java/com/devofure/workoutschedule/ui/main/AddWorkoutScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
