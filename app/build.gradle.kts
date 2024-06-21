e3d4ae774c9cad3e475e69e87b28a94f87bd669e
Erick Chavez <devofure@gmail.com>
2024-06-21 10:12:42 +0200

    fixing keystore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.compose.compiler)
@@ -31,15 +34,42 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            val keyAliasEnv = System.getenv("KEY_ALIAS")
+            val keyPasswordEnv = System.getenv("KEY_PASSWORD")
+            val storeFileEnv = System.getenv("STORE_FILE")
+            val storePasswordEnv = System.getenv("STORE_PASSWORD")
+
+            if (keyAliasEnv != null) {
+                keyAlias = keyAliasEnv
+                keyPassword = keyPasswordEnv
+                storeFile = file(storeFileEnv)
+                storePassword = storePasswordEnv
+            } else {
+                val keystorePropertiesFile = rootProject.file("keystore.properties")
+                val keystoreProperties = Properties()
+                keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
